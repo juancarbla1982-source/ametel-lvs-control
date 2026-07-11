499ce7b9b0eaab90b6c5c37a622ed57fa8ef2f5c
juancarbla1982-source <juancarbla1982@gmail.com>
2026-07-11 08:59:19 +0200

    Add files via upload
    Corrección pantalla blanca y error 404

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='ametel-lvs-control-v020';
+const CACHE='control-ametel-entrega1-v031';
 const BASE='/ametel-lvs-control/';
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.add(BASE)).catch(()=>{}));});
 self.addEventListener('activate',event=>{event.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))]));});
