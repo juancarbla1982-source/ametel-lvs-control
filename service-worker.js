8575ef960b28f7258911ee8b9946028db82a07ab
juancarbla1982-source <juancarbla1982@gmail.com>
2026-07-11 12:15:05 +0200

    Add files via upload
    Entrega 3 - detalle e historial de importaciones

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "control-ametel-v2-importacion";
+const CACHE = "control-ametel-v3-detalle";
 const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
